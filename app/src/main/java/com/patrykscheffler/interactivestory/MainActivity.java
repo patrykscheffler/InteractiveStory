b542ce1312b4995629db86b2b323932c0e7ded3c
Patryk Scheffler <Patryk Scheffler>
2016-01-03 13:11:58 +0100

    Add StoryActivity

--- app/src/main/java/com/patrykscheffler/interactivestory/MainActivity.java
+++ app/src/main/java/com/patrykscheffler/interactivestory/MainActivity.java
@@ -1,5 +1,6 @@
 package com.patrykscheffler.interactivestory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,8 +30,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = mNameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory();
             }
         });
+    }
+
+    private void startStory() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
